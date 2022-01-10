b9438cad8fe53ce9047008173eebd78ccde37a57
Naif <nayef7138@gmail.com>
2022-01-10 15:54:04 +0300

    init the files and folders

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./index.html', './css/*.scss', './css/style.css'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fef1e1',
+        secondary: '#0e223b'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
